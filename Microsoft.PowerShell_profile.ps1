af57981da0cafc77ea3232e34ae50122ccb5567d
Pedro Buffon <henriquebuffon@gmail.com>
2025-12-22 09:39:47 -0300

    personal changes

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -58,7 +58,7 @@ if ($repo_root_Override){
     # then use it instead
     $repo_root = $repo_root_Override
 } else {
-    $repo_root = "https://raw.githubusercontent.com/ChrisTitusTech"
+    $repo_root = "https://raw.githubusercontent.com/PedroBuffon"
 }
 
 # Define the path to the file that stores the last execution time
